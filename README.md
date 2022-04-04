07e88aaa7c26605f6dc6f0f3bcca175ae36c8bf5
Shaun-Gov12 <102986180+Shaun-Gov12@users.noreply.github.com>
2022-04-04 20:10:34 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Design Procedure
+
+## Assumptions
+My understanding of the question was that for each of the 10 locations/positions provided the 10 closest vehicles to those positions
+had to be found. In total this is 100 vehicles.
+
+The "benchmark approach" would have been to loop through all the data in the binary data file and compare each vehicle position 
+with the 10 given positions.
+
+## Solution
+The approach taken in my solution was to find a central position, roughly in the middle of all 10 positions and find the closest 100 
+vehicles to this central position. This method reduces the number of computations by comparing each vehicle's position to a single (central) position. Instead of comparing each vehicle's position to all 10 given positions.
+
+The middle point was found by calculating the center betwen the minimum and maximum lattitude. The same was done for the longitude. This provided the position for the middle point in my solution. 
+
+The C source code was written to follow the algorithm below:
+* Read binary data file and extract the Position ID, Latitude and Longitude for a single vehicle
+* Calculate how far away (the distance) the vehicle is from the mid-point
+* Save the distances and position ID for the first 100 vehicles in 2 seperate arrays
+* (After the first 100 vehicles are read from the data file) If the current distance is smaller than the largest distance from the 100 previous distances save the distance and position ID in arrays
+* All 4 steps above were placed in a loop so that the process would be repeated for vehicles listed in the binary data file
+	
+
+## Possible Improvements
+The solution described above could possibly produce results that are not as accurate at as the "benchmark approach". Speed has been gained at the cost of accuracy. The accuracy of the results could be improved by having 3 or 4 central points instead of just 1 central point. 
+Consider a circle that contains inside it all 10 given positions. The circle could then be divided into 4 sections. Each section would then have it's own central point. The algorithm described in the solution (above) could be adapted to work for each individual section and find the closest 25 vehicles per section.
+
+This method would reduce the inaccuracy of the results because the effect of each central point is limited to a quarter of the entire area. However this method does provide a different set of challenges:
+* Finding the center point of each section is mathematically more intense. Accurate geometry is needed.
+* The C code would need to have additional error checking algorithms for special conditions such as when the algorithm for section 2 selects a vehicle that has already been identified by the algorithm for section 1
---
+++ solution.c
@@ -0,0 +1,130 @@
+#include <math.h>
+#include <stdint.h>
+
+#define No_of_vehicles 100
+
+typedef struct
+{
+	uint32_t pos_ID[No_of_vehicles];
+	float distance[No_of_vehicles];
+
+	float max_distance;
+	uint8_t max_dist_index;
+
+}vehicle_tracker;
+
+
+void extraxtDataFile(uint8_t x, uint32_t* ID, float* lat, float* lng);
+void findMaxDistance(vehicle_tracker* vehicles);
+
+
+int main(void)
+{
+	vehicle_tracker closest_vehicle;
+
+	// ID and location for vehicle
+	uint32_t ID;
+	float lat, lng;
+
+	// Distances from mid-point
+	float delta_lat, delta_lng, distance; 
+	
+	
+
+	// Mid-point co-ordinates
+	float lat_mid = 33.545789;
+	float lng_mid = -98.4465375;
+
+
+	for (uint32_t x = 0; x < 2e6; ++x)
+	{
+		extraxtDataFile(x, &ID, &lat, &lng);
+
+		// Calculate distance from mid-point
+		delta_lat = lat_mid - lat;
+		delta_lng = lng_mid - lng;
+		distance = sqrt( (delta_lat * delta_lat) + (delta_lng * delta_lng) ); // Distance formula
+
+
+		if (x < No_of_vehicles) // Populate arrays 
+		{
+			closest_vehicle.pos_ID[x] = ID;
+			closest_vehicle.distance[x] = distance;
+
+			// Determine vehicle furtheset away first time after all indicies have been populated
+			if (x == (No_of_vehicles - 1) )
+			{
+				findMaxDistance(&closest_vehicle);
+			}
+			 
+		}
+		else
+		{
+			
+			if (distance < closest_vehicle.max_distance)
+			{
+				
+				// Replace vehicle located furthest away with the most recently read vehicle from data file
+				closest_vehicle.pos_ID[closest_vehicle.max_dist_index] = ID;
+				closest_vehicle.distance[closest_vehicle.max_dist_index] = distance;
+
+				// Determine vehicle furtheset away when a new value is placed in 
+				findMaxDistance(&closest_vehicle);
+
+			} // End if
+
+
+		} // End else
+
+		
+
+	} // End loop
+
+
+	return 0;
+} // End main()
+
+
+
+
+
+/* This functions reads the contents of the data file and extracts:
+		--> Position ID
+		--> Latitude
+		--> Longitude
+*/
+void extraxtDataFile(uint8_t x, uint32_t* ID, float* lat, float* lng)
+{
+	// Read data file data for vehicle 'x'
+
+	// Extract ID and store the value in variable ID
+
+	// Extract latitude and store the value in variable lat
+
+	// Extract longitude and store the value in variable lng
+}
+
+
+
+
+
+
+/* This function determines the maximum distance of all the distances in vehicle->distance[]
+   Maximum distance is saved/stored in vehicle->max_distance
+   Index of maximum distance is saved/stored in vehicle->max_dist_index
+*/
+void findMaxDistance(vehicle_tracker* vehicles)
+{
+	vehicles->max_distance = vehicles->distance[0];
+	vehicles->max_dist_index = 0;
+	
+	for (uint8_t i = 1; i < No_of_vehicles; i++)
+	{
+		if ( vehicles->distance[i] > vehicles->max_distance )
+		{
+			vehicles->max_distance = vehicles->distance[i];
+			vehicles->max_dist_index = i;
+		}
+	} //End loop
+
+}
